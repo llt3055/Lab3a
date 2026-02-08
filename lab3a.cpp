0a26fc1bfd7b2a1f018eb052431df654a441b225
llt3055 <llt30555@gmail.com>
2026-02-09 06:52:51 +0800

    Initial setup: Added progrm header and Restaurant struct definition

---
+++ lab3a.cpp
@@ -0,0 +1,20 @@
+// COMSC-210 | Lab 3a | Tianyi Cao
+#include <iostream>
+#include <string>
+#include <vector>
+#include <iomainip>
+
+using namespace std
+
+struct Restaurant {
+    string name;
+    string address;
+    int rating;
+    double price;
+    bool delivery;
+}
+
+int main() {
+    return 0;
+}
+
