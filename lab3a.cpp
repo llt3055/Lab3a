52d421b5611eceefc6e14650e04937f8bf0ee5af
llt3055 <llt30555@gmail.com>
2026-02-09 07:42:57 +0800

    Complete getData implementation with full input validation for all attributes

--- lab3a.cpp
+++ lab3a.cpp
@@ -24,12 +24,22 @@ Restaurant getData() {
     Restaurant temp;
     cout << "Enter restauramt name";
     getline(cin >> temp.name);
+    cout <<"Enter address: ";
+    getline(cin, temp.address);
     cout << "Enter rating (1-5): ";
     while(!(cin >> temp.rating) || temp.rating < 1 || temp.rating>5) {
         cout << "ERROR: Please enter a number between 1 and 5: ";
         cin.ignore();
     }
-
-
+    cin.ignore();
+    cout << "Enter average price: ";
+    getline(cin, temp.price);
+    cout << "Has delivery? (1 for Yes, 0 for No): ";
+    while(!(cin >> temp.delivery) || (temp.delivery != 0 && temp.delivery != 1)) {
+        cout << "ERROR: Enter 1 or 0: ";
+        cin.ignore();
+    }
+    cin.ignore();
+    
     return temp;
 }
