d160be7b68215186d39e41b17ceb0b3f521e6593
llt3055 <llt30555@gmail.com>
2026-02-09 07:12:45 +0800

    Implementng getData function with input validation

--- lab3a.cpp
+++ lab3a.cpp
@@ -6,6 +6,8 @@
 
 using namespace std
 
+Restaurant getData();
+
 struct Restaurant {
     string name;
     string address;
@@ -18,3 +20,16 @@ int main() {
     return 0;
 }
 
+Restaurant getData() {
+    Restaurant temp;
+    cout << "Enter restauramt name";
+    getline(cin >> temp.name);
+    cout << "Enter rating (1-5): ";
+    while(!(cin >> temp.rating) || temp.rating < 1 || temp.rating>5) {
+        cout << "ERROR: Please enter a number between 1 and 5: ";
+        cin.ignore();
+    }
+
+
+    return temp;
+}
