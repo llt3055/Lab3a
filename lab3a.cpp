d044c970aba535c027dee638583bc241335cecbc
llt3055 <llt30555@gmail.com>
2026-02-09 08:07:29 +0800

    Milestone 1: Implemented Restaurant struct, getData with input validation, and debugged some issues

--- lab3a.cpp
+++ lab3a.cpp
@@ -2,11 +2,9 @@
 #include <iostream>
 #include <string>
 #include <vector>
-#include <iomainip>
+#include <iomanip>
 
-using namespace std
-
-Restaurant getData();
+using namespace std;
 
 struct Restaurant {
     string name;
@@ -14,7 +12,9 @@ struct Restaurant {
     int rating;
     double price;
     bool delivery;
-}
+};
+
+Restaurant getData();
 
 int main() {
     return 0;
@@ -23,23 +23,22 @@ int main() {
 Restaurant getData() {
     Restaurant temp;
     cout << "Enter restauramt name";
-    getline(cin >> temp.name);
+    getline(cin, temp.name);
     cout <<"Enter address: ";
     getline(cin, temp.address);
     cout << "Enter rating (1-5): ";
     while(!(cin >> temp.rating) || temp.rating < 1 || temp.rating>5) {
         cout << "ERROR: Please enter a number between 1 and 5: ";
         cin.ignore();
-    }
-    cin.ignore();
+    };
     cout << "Enter average price: ";
-    getline(cin, temp.price);
+    cin >> temp.price;
     cout << "Has delivery? (1 for Yes, 0 for No): ";
     while(!(cin >> temp.delivery) || (temp.delivery != 0 && temp.delivery != 1)) {
         cout << "ERROR: Enter 1 or 0: ";
         cin.ignore();
-    }
+    };
     cin.ignore();
-    
+
     return temp;
-}
+};
