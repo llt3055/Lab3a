662897e1a50203aaf324531d2a01585f7e139220
llt3055 <llt30555@gmail.com>
2026-02-09 08:47:22 +0800

    added some comments to clarify some important blocks and usages and implement getData and display methods by looping through the list

--- lab3a.cpp
+++ lab3a.cpp
@@ -6,6 +6,9 @@
 
 using namespace std;
 
+// Global constant to avoid magic numbers and easily scale the collection size
+const int MAX = 4;
+
 struct Restaurant {
     string name;
     string address;
@@ -13,13 +16,21 @@ struct Restaurant {
     double price;
     bool delivery;
 };
-
+// Function prototypes
 Restaurant getData();
 
-void displayRestaurant(const Restaurant &r);
+void display(const Restaurant &r);
 
 int main() {
     vector<Restaurant> list;
+    for (int i=0;i<MAX;i++){
+        cout << "Restaurant "<<(i+1)<<":"<<endl;
+        list.push_back(getData());
+    }
+
+    for (int i = 0;i<list.size();i++){
+        display(list[i]);
+    }
     return 0;
 }
 
@@ -30,12 +41,14 @@ Restaurant getData() {
     cout <<"Enter address: ";
     getline(cin, temp.address);
     cout << "Enter rating (1-5): ";
+    // Validation loop for rating to ensure it stays within the 1-5 range
     while(!(cin >> temp.rating) || temp.rating < 1 || temp.rating>5) {
         cout << "ERROR: Please enter a number between 1 and 5: ";
         cin.ignore();
     };
     cout << "Enter average price: ";
     cin >> temp.price;
+    // Validation loop for delivery status using integer 0/1 to represent boolean
     cout << "Has delivery? (1 for Yes, 0 for No): ";
     while(!(cin >> temp.delivery) || (temp.delivery != 0 && temp.delivery != 1)) {
         cout << "ERROR: Enter 1 or 0: ";
@@ -47,7 +60,8 @@ Restaurant getData() {
     return temp;
 };
 
-void displayRestaurant(const Restaurant &r) {
+//display: Prints restaurant data in a formatted style.
+void display(const Restaurant &r) {
     cout << "\t> Name: " << r.name << endl;
     cout << "\t> Address: " << r.address << endl;
     cout << "\t> Rating: " << r.rating  << endl;
